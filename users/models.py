8b4c2fcb352ef9b91239cc48863b27f913ba60a8
minki <minki@minkiui-MacBookAir.local>
2023-04-29 18:43:12 +0900

    feat: UserFollow models

--- users/models.py
+++ users/models.py
@@ -20,3 +20,8 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+class UserFollowing(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followings')
+    following_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
