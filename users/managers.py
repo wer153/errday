7bec6793c78e93443effff9304343a49b2419ff0
minki <minki@minkiui-MacBookAir.local>
2023-04-29 18:36:32 +0900

    fix: UserManager to inherit from models.Manager instead of UserManager; not matching with custom User model

--- users/managers.py
+++ users/managers.py
@@ -1,5 +1,29 @@
-from django.contrib.auth.models import UserManager as AuthUserManger
+from django.db import models
+from django.contrib.auth.hashers import make_password
 
 
-class UserManager(AuthUserManger):
-    pass
+class UserManager(models.Manager):
+    def create(self, email, password, date_of_birth=None, is_admin=False, is_superuser=False):
+        self.normalize_email(email)
+        return super().create(
+            email=email,
+            password=make_password(password),
+            date_of_birth=date_of_birth,
+            is_admin=is_admin,
+            is_superuser=is_superuser,
+        )
+
+    @classmethod
+    def normalize_email(cls, email):
+        """
+        Normalize the address by lowercasing the domain part of the email
+        address.
+        """
+        email = email or ''
+        try:
+            email_name, domain_part = email.strip().rsplit('@', 1)
+        except ValueError:
+            pass
+        else:
+            email = '@'.join([email_name, domain_part.lower()])
+        return email
