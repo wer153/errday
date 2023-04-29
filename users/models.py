1f4876c84db963c7d77cd734d5a8126867ac4d6e
minki <minki@minkiui-MacBookAir.local>
2023-04-29 18:35:17 +0900

    feat: user date of birth to be nullable

--- users/models.py
+++ users/models.py
@@ -10,7 +10,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         max_length=255,
         unique=True,
     )
-    date_of_birth = models.DateField()
+    date_of_birth = models.DateField(null=True)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
 
