bf0b3eb9e2a1b083ef06420d6734ad60fef3862f
minki <minki@minkiui-MacBookAir.local>
2023-04-30 18:07:41 +0900

    fix: is_admin -> is_staff in user model

--- users/models.py
+++ users/models.py
@@ -12,7 +12,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     )
     date_of_birth = models.DateField(null=True)
     is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
