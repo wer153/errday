94417c960c22b6bed28a2b8838a62d9d3837bba1
minki <minki@minkiui-MacBookAir.local>
2023-05-07 18:05:35 +0900

    ref: inherit from BaseUserManager

--- users/managers.py
+++ users/managers.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.base_user import BaseUserManager
 
 
-class UserManager(models.Manager):
+class UserManager(BaseUserManager):
     def create(self, email, password, date_of_birth=None, is_staff=False, is_superuser=False):
         self.normalize_email(email)
         return super().create(
@@ -12,21 +13,3 @@ class UserManager(models.Manager):
             is_staff=is_staff,
             is_superuser=is_superuser,
         )
-
-    @classmethod
-    def normalize_email(cls, email):
-        """
-        Normalize the address by lowercasing the domain part of the email
-        address.
-        """
-        email = email or ''
-        try:
-            email_name, domain_part = email.strip().rsplit('@', 1)
-        except ValueError:
-            pass
-        else:
-            email = '@'.join([email_name, domain_part.lower()])
-        return email
-
-    def get_by_natural_key(self, email):
-        return self.get(email=email)
