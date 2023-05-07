cd8d751428c58cb131958b7697939d2302df53cb
minki <minki@minkiui-MacBookAir.local>
2023-05-07 16:39:52 +0900

    feat: profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
@@ -25,3 +27,10 @@ class User(AbstractBaseUser, PermissionsMixin):
 class UserFollowing(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followings')
     following_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
+
+
+class Profile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    nick_name = models.CharField(max_length=20, unique=True)
