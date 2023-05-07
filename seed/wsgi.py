0a2271734e2ef9cd9a5a18133235d34d8cda1078
minki <minki@minkiui-MacBookAir.local>
2023-05-07 16:26:43 +0900

    ref: app name errday -> seed

--- seed/wsgi.py
+++ seed/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for errday project.
+WSGI config for seed project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'errday.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'seed.settings')
 
 application = get_wsgi_application()
