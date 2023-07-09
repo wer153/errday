8ae9407cd0d0b30eca08ae0e892c78b84c245418
minki <minki@minkiui-MacBookAir.local>
2023-07-09 16:31:16 +0900

    ADD: post model

--- calenders/models.py
+++ calenders/models.py
@@ -1,3 +1,10 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    post_datetime = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField()
+    thumbnail = models.ImageField(blank=True, null=True)
