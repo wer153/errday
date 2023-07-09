2d1e698c6ce3ed04cf3c1c8a3af8880cb85b7112
minki <minki@minkiui-MacBookAir.local>
2023-07-09 18:52:45 +0900

    ADD: calender model

--- calenders/models.py
+++ calenders/models.py
@@ -7,10 +7,21 @@ from django.db import models
 User = get_user_model()
 
 
+class Calender(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    owner = models.ForeignKey(to=User, related_name='calenders', on_delete=models.PROTECT)
+
+
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    post_date = models.DateField(unique=True)
+    post_date = models.DateField()
     image = models.ImageField()
     thumbnail = models.ImageField(blank=True, null=True)
     emoji = models.CharField(blank=True, max_length=10)
     user = models.ForeignKey(to=User, related_name='posts', on_delete=models.PROTECT)
+    calender = models.ForeignKey(
+        to=Calender,
+        related_name='posts',
+        on_delete=models.PROTECT,
+        unique_for_date='post_date',
+    )
