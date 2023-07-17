71cc725199b617dbe889b3e8e2c8e93bd56de96e
minki <minki@minkiui-MacBookAir.local>
2023-07-17 17:36:20 +0900

    CHG: user API

--- seed/apis.py
+++ seed/apis.py
@@ -1,6 +1,8 @@
 from ninja import NinjaAPI
 
-from calenders.apis import router
+from calenders.apis import router as calender_router
+from users.apis import router as user_router
 
 api = NinjaAPI()
-api.add_router('/calenders', router)
+api.add_router('/calenders', calender_router)
+api.add_router('/users', user_router)
---
+++ users/apis.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import get_user_model, login
+from django.shortcuts import get_object_or_404
+from ninja import Router
+
+from users.services import signup
+
+
+User = get_user_model()
+
+router = Router()
+
+
+@router.post('/signup')
+def user_signup(request, email: str, nick_name: str):
+    user = signup(email=email, nick_name='', image=None)
+    return email
+
+
+@router.get('/signin')
+def user_signin(request, email: str):
+    user = get_object_or_404(
+        User,
+        email=email,
+    )
+    login(request=request, user=user)
+    return None
