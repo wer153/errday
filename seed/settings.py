383b7981445eaec6d6fbb09e901387dbd50e9b65
minki <minki@minkiui-MacBookAir.local>
2023-07-19 16:50:25 +0900

    CHG: set public-read option

--- seed/settings.py
+++ seed/settings.py
@@ -26,11 +26,12 @@ SECRET_KEY = 'django-insecure-nt#k9j!8i%twi8ru%6@^@s-izbftg3m_6#d2c12qbrpu!x(5c#
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STORAGES = {"default": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"}}
 AWS_S3_SECURE_URLS = False       # use http instead of https
-AWS_QUERYSTRING_AUTH = False     # don't add complex authentication-related query parameters for requests
-
+AWS_QUERYSTRING_AUTH = True     # don't add complex authentication-related query parameters for requests
+AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_ACCESS_KEY_ID = 'AKIAVX5W5R3JAVAOM6H2'
 AWS_S3_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'narrabeen-seed'
+AWS_S3_REGION_NAME = 'ap-northeast-2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
