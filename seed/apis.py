c077e22000797080b755025cd42a497325e6f7d2
minki <minki@minkiui-MacBookAir.local>
2023-07-26 17:06:34 +0900

    fix: set csrf on main router

--- seed/apis.py
+++ seed/apis.py
@@ -3,6 +3,6 @@ from ninja import NinjaAPI
 from calenders.apis import router as calender_router
 from users.apis import router as user_router
 
-api = NinjaAPI()
+api = NinjaAPI(csrf=True)
 api.add_router('/calenders', calender_router)
 api.add_router('/users', user_router)
